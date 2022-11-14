cd52f632e9747c5b89f8414a892bc87177a8001d
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-11-14 12:39:45 -0800

    delete eslintrc

--- src/login.js
+++ src/login.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import PageFooter from './pages/parts/PageFooter.js';
+import React from "react";
+import PageFooter from "./pages/parts/PageFooter.js";
 
 const LogIn = () => {
     return (
         <div>
-        <div className="container text-center">
-            <div className="row d-flex justify-content-center header-row">
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-                <h1>Log In</h1>
-            </div>
-            <div className="col-md-3"></div>
-            </div>
-            <div className="row d-flex justify-content-center">
-                <div className="col-md-3"></div>
-                <div className="col-md-6">
-                    <p>Hi, Ceci!</p>
+            <div className="container text-center">
+                <div className="row d-flex justify-content-center header-row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <h1>Log In</h1>
+                    </div>
+                    <div className="col-md-3"></div>
                 </div>
-                <div className="col-md-3"></div>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <p>Hi, Ceci!</p>
+                    </div>
+                    <div className="col-md-3"></div>
+                </div>
             </div>
+            <PageFooter></PageFooter>
         </div>
-        <PageFooter></PageFooter>
-        </div>
-    )
-}
+    );
+};
 
 export default LogIn;
