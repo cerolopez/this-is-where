7a827fcd97e9b44c6beec5b676f3de5ee51082dd
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-11-15 10:15:47 -0800

    update login and settings

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,6 +1,22 @@
 import React from "react";
 import PageFooter from "./pages/parts/PageFooter.js";
 
+const LoginField = ({ _for, _label, _type }) => {
+    return (
+    <div className="mb-3">
+        <label for={"input" + _for} className="form-label">
+            {_label}
+        </label>
+        <input
+            type={_type}
+            className="form-control"
+            id={"input" + _label}
+            required
+        />
+    </div>
+    );
+};
+
 const LogIn = () => {
     return (
         <div>
@@ -15,7 +31,14 @@ const LogIn = () => {
                 <div className="row d-flex justify-content-center">
                     <div className="col-md-3"></div>
                     <div className="col-md-6">
-                        <p>Hi, Ceci!</p>
+                        <form>
+                        <LoginField _for="Username" _label="Username" _type="text"> </LoginField>
+
+                        <LoginField _for="Password" _label="Password" _type="password"> </LoginField>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </form>
                     </div>
                     <div className="col-md-3"></div>
                 </div>
--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,28 +1,75 @@
-import React from 'react';
-import PageTemplate from "./pages/PageTemplate.js"
+import React from "react";
+import PageTemplate from "./pages/PageTemplate.js";
+
+const UserSetting = ({setting}) => {
+    return (
+        <div className="input-group flex-nowrap">
+            <span className="input-group-text userSetting" id="addon-wrapping">
+                Change {setting} 
+            </span>
+            <input
+                type="text"
+                className="form-control userSetting"
+                placeholder={`New ${setting}`}
+                aria-label={`New ${setting}`}
+                aria-describedby="addon-wrapping"
+            />
+        </div>
+    );
+};
 
 const Settings = () => {
     return (
         <div>
-        <div className="container">
-            <div className="row d-flex header-row">
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-                <h1>Settings</h1>
-            </div>
-            <div className="col-md-3"></div>
-            </div>
-            <div className="row d-flex">
-                <div className="col-md-3"></div>
-                <div className="col-md-6">
-                    <p>Add settings form here</p>
+            <div className="container">
+                <div className="row d-flex header-row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <h1>Account Settings</h1>
+                    </div>
+                    <div className="col-md-3"></div>
                 </div>
-                <div className="col-md-3"></div>
-            </div>
+                <div className="row d-flex">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <form id="submitUsernameForm" name="submitUsernameForm">
+                        <UserSetting setting="username"></UserSetting>
+                        <UserSetting setting="email address"></UserSetting>
+                            <button
+                                type="submit"
+                                id="submitUsernameButton"
+                                className="btn btn-primary submitButton"
+                            >
+                                Submit
+                            </button>
+                        </form>
+                        <br/>
+                        <div className="card userSetting">
+                            <div className="card-header">Remove account</div>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    Do you want to delete your account?
+                                </h5>
+                                <p className="card-text">
+                                    Please click submit only if you are sure -
+                                    this will permanently delete your account
+                                    and all of your posts!
+                                </p>
+                                <button
+                                    className="btn btn-danger"
+                                    id="deleteButton"
+                                >
+                                    Delete Account
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3"></div>
+                </div>
             </div>
             <PageTemplate></PageTemplate>
         </div>
-    )
-}
+    );
+};
 
 export default Settings;
