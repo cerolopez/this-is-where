93367ec801963322eb518ab5f65012eb93965799
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-11-15 12:03:08 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ let router = express.Router();
 
 router.post('/newPost', async (req, res) => {
   const postInfo = req.body;
-  const newPost = await postsDB.getPosts(postInfo);
+  const newPost = await postsDB.createPost(postInfo);
 
   if (newPost) {
     return res.json({postCreated: true, err: null});
@@ -16,5 +16,74 @@ router.post('/newPost', async (req, res) => {
     res.json({postCreated: false, err: 'error creating new post'});
   }
 })
+
+// URL will have postID in URL as query
+router.get('/findPost', async (req, res) => {
+  const postID = req.query.id;
+  const foundPost = await postsDB.getPost(postID);
+
+  if (foundPost) {
+    return res.json(foundPost);
+  } else {
+    res.json({postFound: false, err: 'post not found'});
+  }
+})
+
+router.post('/editPost', async (req, res) => {
+  const postID = req.query.id;
+  const postEdits = req.body;
+  const editedPost = await postsDB.editPost(postID, postEdits);
+
+  if (editedPost) {
+    return res.json({postEdited: true, err: null});
+  } else {
+    return res.json({postEdited: false, err: 'error editing post'});
+  }
+})
+
+router.get('/deletePost', async (req, res) => {
+  const postID = req.query.id;
+  const isDeleted = await postsDB.deletePost(postID);
+
+  if (isDeleted) {
+    return res.json({postDeleted: true, err: null});
+  } else {
+    return res.json({postDeleted: false, err: 'error deleting post'});
+  }
+})
+
+router.post('/flagPost', async (req, res) => {
+  const postID = req.query.id;
+  const isFlagged = await postsDB.flagPost(postID);
+
+  if (isFlagged) {
+    return res.json({postFlagged: true, err: null});
+  } else {
+    return res.json({postFlagged: false, err: 'error flagging post'});
+  }
+})
+
+router.post('/likePost', async (req, res) => {
+  const postID = req.query.id;
+  const isLiked = await postsDB.likePost(postID);
+
+  if (isLiked) {
+    return res.json({postLiked: true, err: null});
+  } else {
+    return res.json({postLiked: false, err: 'error liking post'});
+  }
+})
+
+router.post('/unlikePost', async (req, res) => {
+  const postID = req.query.id;
+  const isUnliked = await postsDB.unlikePost(postID);
+
+  if (isUnliked) {
+    return res.json({postUnliked: true, err: null});
+  } else {
+    return res.json({postUnliked: false, err: 'error unliking post'});
+  }
+})
+
 
 export default router;
