0dbf1b8178da8a6e4f444bb1a3352567c9b83188
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-11-25 18:18:27 -0800

    Update FullPostComponent.js

--- frontend/src/components/FullPostComponent.js
+++ frontend/src/components/FullPostComponent.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function FullPostComponent(post) {
+    const [like, setLike] = useState();
 
     const fullDate = new Date(post.date);
     const dateString = fullDate.toDateString();
