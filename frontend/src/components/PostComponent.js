8888997d8d7b41188816d1fa394902a2d9776326
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-11-27 18:18:04 -0800

    Remove handleLinkClick

--- frontend/src/components/PostComponent.js
+++ frontend/src/components/PostComponent.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
 import PropTypes from "prop-types";
-import {useNavigate, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function PostComponent({ post, likeCount, fullDisplay, reloadData }) {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     let dateFormat;
     const timestamp = post.date;
     dateFormat = new Date(timestamp);
@@ -43,11 +43,11 @@ function PostComponent({ post, likeCount, fullDisplay, reloadData }) {
     }, []
     );
         
-    function handleLinkClick(evt) {
-        console.log("I'm in handleLinkClick");
-        evt.preventDefault();
-        navigate(`/view-post?id=${post._id}`, {replace: false});
-    };
+    // function handleLinkClick(evt) {
+    //     console.log("I'm in handleLinkClick");
+    //     evt.preventDefault();
+    //     navigate(`/view-post?id=${post._id}`, {replace: false});
+    // };
 
     async function sendFavoriteToDB() {
         if (!isFavoritedByUser) {
@@ -94,7 +94,12 @@ function PostComponent({ post, likeCount, fullDisplay, reloadData }) {
                         }}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={isFavoritedByUser ? "yellow" : "black"} className="bi bi-star" viewBox="0 0 16 16">
   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
 </svg></Link>&nbsp;&nbsp;
-                         <Link onClick={event => handleLinkClick(event)}>{post.location}</Link></h4>
+                         {/* <Link onClick={event => handleLinkClick(event)}>{post.location}</Link></h4> */}
+                         <Link to={{
+                            pathname: "/view-post", 
+                            search: `?id=${post._id}`, 
+                            // state: "Hello woueirld"
+                            }}>{post.location}</Link></h4>
                          <div className="row justify-content-start">
                             <div className="col-md-3">
                                 <span className="badge bg-secondary">{post.city}</span>
