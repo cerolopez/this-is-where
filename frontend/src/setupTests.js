08e1cf46309a1d972842111e384431a9c3aedefb
YixiangXie-NEU <113476679+YixiangXie-NEU@users.noreply.github.com>
2022-12-01 20:05:27 -0800

    review: remove unused file

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,3 +1,4 @@
+// CODE REVIEW: it is better to remove unused create-react-app files
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
