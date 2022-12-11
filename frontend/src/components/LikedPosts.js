6fb28a88d74d470988e57744f4e1d3783fb95390
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-12-11 12:08:47 -0800

    Create LikedPosts.js

---
+++ frontend/src/components/LikedPosts.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from "react";
+import PostComponent from "./PostComponent.js";
+
+function LikedPosts() {
+
+    const [fullDisplay, setFullDisplay] = useState("none");
+    const [likedPosts, setLikedPosts] = useState([{ post: "" }]);
+    const [loadDisplay, setloadDisplay] = useState("block");
+    const [usersLikes, setUsersLikes] = useState([]);
+    const [usersFavorites, setUsersFavorites] = useState([]);
+
+    async function reloadData() {
+        let data;
+
+        try {
+            const res = await fetch("/getLikedPosts", {
+                method: "GET",
+            });
+            data = await res.json();
+        } catch (e) {
+            console.log("error downloading data: ", e);
+            return false;
+        }
+
+        setLikedPosts(data);
+
+        async function getLikesAndFavorites() {
+            const likes = await fetch("/getLikes");
+            const likesJson = await likes.json();
+
+            const likesArray = likesJson.at(0).liked_posts;
+            setUsersLikes(likesArray);
+            const favorites = await fetch("/getFavorites");
+            const favoritesJson = await favorites.json();
+            const favoritesArray = favoritesJson.at(0).favorited_posts;
+            setUsersFavorites(favoritesArray);
+        }
+        getLikesAndFavorites();
+
+        setloadDisplay("none");
+        setFullDisplay("block");
+    }
+
+    useEffect(() => {
+
+        reloadData();
+    }, []);
+
+    return (
+        <>
+            <div className="row d-flex justify-content-center" id="post">
+                <div className="col-md-1">
+                    <p style={{ display: `${loadDisplay}` }}>Loading...</p>
+                </div>
+            </div>
+            <div className="PostsFeed">
+                {likedPosts.map((p, i) => (
+                    <PostComponent
+                        key={`object_${i}`}
+                        post={p}
+                        fullDisplay={fullDisplay}
+                        usersLikes={usersLikes}
+                        usersFavorites={usersFavorites}
+                    ></PostComponent>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default LikedPosts;
