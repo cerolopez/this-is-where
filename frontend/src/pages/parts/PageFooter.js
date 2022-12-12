f5df59cc80a53b5fa76c50febe8ab155fb3ba2c6
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-12-11 19:20:11 -0800

    Update PageFooter.js

--- frontend/src/pages/parts/PageFooter.js
+++ frontend/src/pages/parts/PageFooter.js
@@ -8,12 +8,12 @@ function PageFooter() {
             <footer className="bg-light text-center">
             <div className="row">
                 <div className="col-md-12 d-flex justify-content-center">
-                    <h4>2022 This Is Where &copy;</h4>
+                    <p>2022 This Is Where &copy;</p>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-12 d-flex justify-content-center">
-                    <h4>Made by Cecilia Lopez and Armen Sarkisian</h4>
+                    <p>Made by Cecilia Lopez and Armen Sarkisian</p>
                 </div>
             </div>
         </footer>
