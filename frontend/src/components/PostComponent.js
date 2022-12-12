9d41bc2bbd67f2f3cdd026cdf6beca14fa25ea99
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-12-11 17:59:06 -0800

    Update PostComponent.js

--- frontend/src/components/PostComponent.js
+++ frontend/src/components/PostComponent.js
@@ -73,7 +73,7 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                                     </svg>
                                 </Link>
                                 &nbsp;&nbsp;
-                                <a aria-label="View full post" href={`/view-post?id=${post._id}`}>{post.location}</a>
+                                <a aria-label="View full post" href={`/view-post?id=${post._id}`}>{post.location || "post location"}</a>
                             </h2>
                             <div className="row justify-content-start">
                                 <div className="col-md-12">
