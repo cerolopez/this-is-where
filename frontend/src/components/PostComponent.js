e1ef7de13892207e66a7c5268fb273fc86737adf
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-12-11 18:21:25 -0800

    Fixed axe id attribute issues

--- frontend/src/components/PostComponent.js
+++ frontend/src/components/PostComponent.js
@@ -42,10 +42,10 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
 
     return (
         <div className="container postFeed" style={{ display: `${fullDisplay}` }}>
-            <div className="row d-flex justify-content-center" id="post">
+            <div className="row d-flex justify-content-center post">
                 <div className="col-md-3"></div>
                 <div className="col-md-6">
-                    <div className="card" id="postID">
+                    <div className="card postID">
                         <div className="card-body">
                             <h2>
                                 <Link
@@ -58,7 +58,6 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                                     aria-label="favorite post"
                                 >
                                     <svg
-                                        id="favorite-icon"
                                         aria-label="favorite post"
                                         xmlns="http://www.w3.org/2000/svg"
                                         width="25"
@@ -68,7 +67,7 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                                                 ? "#F6BE00"
                                                 : "black"
                                         }
-                                        className="bi bi-star"
+                                        className="bi bi-star favorite-icon"
                                         viewBox="0 0 16 16"
                                     >
                                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
@@ -91,7 +90,7 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                             <br />
                             <div className="row">
                                 <div className="col-md-12">
-                                    <h4 id="post-subtitle">
+                                    <h4 className="post-subtitle">
                                         Posted on {dateFormat.toDateString()} by{" "}
                                         <strong>{post.username}</strong>
                                     </h4>
@@ -102,7 +101,7 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                 </div>
                 <div className="col-md-3"></div>
             </div>
-            <div className="row d-flex justify-content-center" id="like">
+            <div className="row d-flex justify-content-center like">
                 <div className="col-md-3"></div>
                 <div className="col-md-6">
 
@@ -118,10 +117,11 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                                 );
                                 sendLikeToDB();
                                 }}
+                        aria-label="like post"
                     >
 
                         <svg
-                            id="heart-icon"
+                            aria-label="heart icon"
                             xmlns="http://www.w3.org/2000/svg"
                             width="16"
                             height="16"
@@ -130,7 +130,7 @@ function PostComponent({ post, fullDisplay, usersLikes, usersFavorites }) {
                                     ? "#BD2020"
                                     : "black"
                                 }
-                            className="bi bi-heart"
+                            className="bi bi-heart heart-icon"
                             viewBox="0 0 16 16"
                         >
                             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
